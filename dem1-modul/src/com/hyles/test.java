2bcf77fd3262b23c5644ff55d82843607122a9ae
yzhyrz <2958671279@qq.com>
2023-06-29 19:19:46 +0800

    第一次更新

--- dem1-modul/src/com/hyles/test.java
+++ dem1-modul/src/com/hyles/test.java
@@ -5,6 +5,6 @@ package com.hyles;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello github");
+        System.out.println("hello ");
     }
 }
